5441a1d9a34a5f5e640be701aa1ae5aff84b280a
tsamantanis <philip.tsamantanis@icloud.com>
2020-12-30 13:16:48 -0800

    Capitalize Words

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 capitalize = (str) => str.substring(0, 1).toUpperCase() + str.substring(1, str.length);
 
 allCaps = (str) => str.toUpperCase();
+
+capitalizeWords = (str) => str.split(' ').map((word, i) => capitalize(word)).join(' ');
