20de602d59c24c6067ab5393d2efd30f45b6e09a
tsamantanis <philip.tsamantanis@icloud.com>
2020-12-30 13:21:48 -0800

    Capitalize Headline

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ capitalize = (str) => str.substring(0, 1).toUpperCase() + str.substring(1, str.l
 allCaps = (str) => str.toUpperCase();
 
 capitalizeWords = (str) => str.split(' ').map((word, i) => capitalize(word)).join(' ');
+
+capitalizeHeadline = (str) => {
+    const notCaps = ['the', 'in', 'a', 'an', 'and', 'but', 'for', 'at', 'by'];
+    return str.split(' ').map((word) => !notCaps.includes(word) ? capitalize(word) : word).join(' ');
+}
